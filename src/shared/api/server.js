e5f8b1c9dd6f8306c6a455cbd578cdcd866584b1
Robert Queiroz <139017352+robertqsilva@users.noreply.github.com>
2024-02-10 13:34:41 -0300

    feat: adicionando dominio no cors

--- src/shared/api/server.js
+++ src/shared/api/server.js
@@ -7,7 +7,10 @@ const axios = require("axios");
 const app = express();
 
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://sniperbuda.onrender.com',
+  optionsSuccessStatus: 200 
+}));
 
 app.use(express.json());
 app.use(router);
